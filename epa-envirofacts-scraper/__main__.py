86549d3db1ca5ede48afa0b6beb113ee1b01f730
doug <doug.kelkhoff@gmail.com>
2017-02-15 09:43:36 -0800

    added command line argument to limit the number of tables to be downloaded for testing.

--- epa-envirofacts-scraper/__main__.py
+++ epa-envirofacts-scraper/__main__.py
@@ -9,10 +9,11 @@ logger = initLogger("__main__.py")
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-o", "--output", help="directory to store archived datasets to.", default=None)
+    parser.add_argument("-l", "--limit", help="limit the number of tables to download.", type=int, default=None)
     args = parser.parse_args()
 
     logger.info("Starting archiving process")
-    archive(get_table_names(), limit=1, data_dir=args.output)
+    archive(get_table_names(), limit=args.limit, data_dir=args.output)
     logger.info("Done archiving process")
 
 if __name__ == '__main__':
